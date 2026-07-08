e0cf7c2b567c808d2bb92275eda9657d45b7d796
Akashsingh86500 <akchaudhary86500@gmail.com>
2026-07-09 00:10:21 +0530

    Time: 2 ms (87.16%) | Memory: 43.8 MB (57.25%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] count = new int[26];
+        boolean[] visited = new boolean[26];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            count[c - 'a']--;
+            if (visited[c - 'a']) {
+                continue;
+            }
+            while (!stack.isEmpty()
+                    && stack.peek() > c
+                    && count[stack.peek() - 'a'] > 0) {
+                visited[stack.pop() - 'a'] = false;
+            }
+
+            stack.push(c);
+            visited[c - 'a'] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char ch : stack) {
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
