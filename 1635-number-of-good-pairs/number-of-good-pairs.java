77ce47647739cb2fe46473468ddb79f17b84ed3f
Akashsingh86500 <akchaudhary86500@gmail.com>
2026-08-03 00:40:27 +0530

    Time: 1 ms (87.20%) | Memory: 42.8 MB (62.75%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                count+=map.get(nums[i]);
+                int freq=map.get(nums[i]);
+                map.put(nums[i],freq+1);
+                //count+=map.get(nums[i]);
+            }
+            else map.put(nums[i],1);
+        }
+        return count;
+    }
+}
